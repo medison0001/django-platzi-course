8413f451161ff6a0506551449dc3566eb03cc8ce
medison0001 <12345678+medison0001@users.noreply.github.com>
2025-06-17 15:33:31 -0500

    Inicio del proyecto: configuración de templates, formulario y vistas para productos

--- products/forms.py
+++ products/forms.py
@@ -12,6 +12,7 @@ class ProductFrom(forms.Form):
         Product.objects.create(
             name = self.cleaned_data['name'],
             description = self.cleaned_data['description'],
+            price = self.cleaned_data['price'],
             available = self.cleaned_data['available'],
             photo = self.cleaned_data['photo'],
         )
--- products/views.py
+++ products/views.py
@@ -2,13 +2,19 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 from .forms import ProductFrom
+from .models import Product
 
 # Create your views here.
 class ProductFormView(generic.FormView):
     template_name = "products/add_producto.html"
     form_class = ProductFrom
-    success_url = reverse_lazy("add_product")
+    success_url = reverse_lazy("list_product")
 
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
+
+class ProductListView(generic.ListView):
+    model = Product
+    template_name = 'products/list_product.html'
+    context_object_name = 'products'
